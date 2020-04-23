f54761c171b3933f66f43a40da95665ea0a1a558
Nik <miniakink@gmail.com>
2020-04-24 01:53:10 +0300

    работает 1

--- homework.py
+++ homework.py
@@ -13,8 +13,9 @@ def get_status(user_id):
         'access_token': os.getenv('token_vk'),
         'fields': 'online',
     }
-    friends_list = requests.post('https://api.vk.com/method/users.get', params=data)
-    return friends_list
+    response = requests.post('https://api.vk.com/method/users.get', params=data)
+    online_or_not = response.json()['response'][0]['online']
+    return online_or_not
 
 
 def sms_sender(sms_text):
